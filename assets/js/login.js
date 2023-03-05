f55970378300e602dfd275a96fd07647ae999f3f
David Chibueze Ndubuisi <davydocsurg@gmail.com>
2023-03-05 20:44:31 +0100

    Revert "Cinehub movies upload form 4"

---
+++ assets/js/login.js
@@ -0,0 +1,77 @@
+// import { setError, setSuccess } from "./error.js";
+
+import {
+  validateEmailLogin,
+  validatePasswordLogin,
+  validateBoth,
+} from "./validate.js";
+
+// const eMail = document.getElementById("email");
+// const passWord = document.getElementById("password");
+// const btn = document.querySelector("button");
+
+// const emailIdentity = localStorage.getItem("email");
+// const passWordLogin = localStorage.getItem("password");
+// const userNameIdentity = localStorage.getItem("username");
+
+// console.log(userNameIdentity);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  validateEmailLogin();
+  validatePasswordLogin();
+  validateBoth();
+});
+
+// const setError = (element, message) => {
+//   const formControl = element.parentElement;
+//   const errorDisplay = formControl.querySelector("small");
+
+//   errorDisplay.textContent = message;
+//   formControl.classList.add("error");
+// };
+
+// const setSuccess = (element, message) => {
+//   const formControl = element.parentElement;
+//   const errorDisplay = formControl.querySelector("small");
+
+//   errorDisplay.innerText = "";
+//   formControl.classList.remove("error");
+//   formControl.classList.add("success");
+// };
+
+// const validateEmailLogin = () => {
+//   const eMailValue = eMail.value.trim();
+//   if (eMailValue === "") {
+//     setError(eMail, "Please enter your email address");
+//   } else if (eMail.value !== emailIdentity) {
+//     setError(eMail, "The email address you entered is incorrect");
+//   } else if (eMail.value === emailIdentity) {
+//     setSuccess(eMail);
+//   }
+// };
+
+// const validatePasswordLogin = () => {
+//   const passWordValue = passWord.value.trim();
+//   if (passWordValue === "") {
+//     setError(passWord, "Please enter your password");
+//   } else if (passWordValue < 8 || passWordValue > 15) {
+//     setError(passWord, "Password must be between 8 and 15 characters");
+//   } else if (passWordValue !== passWordLogin) {
+//     setError(passWord, "The password you entered is incorrect");
+//   } else if (passWordValue === passWordLogin) {
+//     setSuccess(passWord);
+//     console.log("Successful");
+//   }
+// };
+
+// const validateBoth = () => {
+//   if (eMail.value === emailIdentity && passWord.value === passWordLogin) {
+//     setSuccess(eMail);
+//     setSuccess(passWord);
+//     setTimeout(() => {
+//       alert(`${userNameIdentity}, You have been logged in`);
+//     }, 1000);
+//   }
+// };
