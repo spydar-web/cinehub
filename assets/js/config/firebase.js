018e74c047940cf45dad059c60f3b81e61007adf
davydocsurg <davydocsurg@gmail.com>
2023-05-08 16:00:42 +0100

    setup dev environment, add firebase config #9

---
+++ assets/js/config/firebase.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: "cinehub-mv.firebaseapp.com",
+    projectId: "cinehub-mv",
+    storageBucket: "cinehub-mv.appspot.com",
+    messagingSenderId: "32441279740",
+    appId: "1:32441279740:web:ab48ad6ba68b8e89e72379",
+    measurementId: "G-6E3GFT3H0T",
+};
